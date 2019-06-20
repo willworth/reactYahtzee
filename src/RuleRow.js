c8e91ac509b01de040e35d177b529f05638885a2
Will Worth <will.worth@gmail.com>
2019-06-20 13:05:17 +0200

    finished

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -4,14 +4,14 @@ import "./RuleRow.css";
 class RuleRow extends Component {
   render() {
     const { score, name, doScore, description } = this.props;
-    const disabled = this.props.score !== undefined;
+    const disabled = score !== undefined;
     return (
       <tr
         className={`RuleRow RuleRow-${disabled ? "disabled" : "active"}`}
         onClick={disabled ? null : doScore}
       >
-        <td className="RuleRow-name">{name}</td>
-        <td className="RuleRow-score">{disabled ? score : description}</td>
+        <td className='RuleRow-name'>{name}</td>
+        <td className='RuleRow-score'>{disabled ? score : description}</td>
       </tr>
     );
   }
